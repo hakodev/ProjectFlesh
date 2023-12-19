760ecafee2b8ab395a981637dd512e61f07ff509
Ahmet Hakan Koç <ahmetkoc.business@gmail.com>
2023-12-19 17:17:15 +0100

    Placeholder assets

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    private void Awake() {}
+    private void Start() {}
+    private void Update() {}
+    private void FixedUpdate() {}
+    private void LateUpdate() {}
+}
