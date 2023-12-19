d81fccdacf81a34ab86f9f3ba3d490748027dac0
Ahmet Hakan Koç <ahmetkoc.business@gmail.com>
2023-12-19 18:22:07 +0100

    Implemented moving and jumping

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,9 +1,38 @@
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-    private void Awake() {}
-    private void Start() {}
-    private void Update() {}
-    private void FixedUpdate() {}
-    private void LateUpdate() {}
+    private Rigidbody2D rb2d;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpForce;
+    private float horizontalAxis;
+    private bool jumpPressed;
+    private bool isGrounded;
+
+    private void Awake() {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update() {
+        horizontalAxis = Input.GetAxisRaw("Horizontal");
+        jumpPressed = Input.GetButtonDown("Jump");
+        if(jumpPressed && isGrounded) {
+            rb2d.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+        }
+    }
+
+    private void FixedUpdate() {
+        rb2d.velocity = new Vector2(horizontalAxis * moveSpeed, rb2d.velocity.y);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if(collision.gameObject.CompareTag("Ground")) {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision) {
+        if(collision.gameObject.CompareTag("Ground")) {
+            isGrounded = false;
+        }
+    }
 }
