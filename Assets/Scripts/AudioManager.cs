b81a69abe5213fee0d191655c5eb8437853aa154
Ahmet Hakan Koç <ahmetkoc.business@gmail.com>
2024-02-03 15:42:11 +0100

    Audio players implemented

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+    public static AudioManager Ins { get; private set; }
+
+    #region Audio dump
+
+    [field: Header("Music")] // Add music below
+    [field: SerializeField] public AudioClip GameplayAmbienceLoop { get; private set; }
+
+    [field: Header("SFX")] // Add sfx below
+    [field: SerializeField] public AudioClip BoneBreaking { get; private set; }
+    [field: SerializeField] public AudioClip CatStab { get; private set; }
+    [field: SerializeField] public AudioClip GlassShatter { get; private set; }
+    [field: SerializeField] public AudioClip HammerHitCat { get; private set; }
+    [field: SerializeField] public AudioClip HeartbeatLoopFaster { get; private set; }
+    [field: SerializeField] public AudioClip HeartbeatLoop { get; private set; }
+
+    [Header("Audio players")]
+    [SerializeField] private AudioSource musicPlayer;
+    [SerializeField] private AudioSource sfxPlayer;
+
+    #endregion
+
+    private void Awake() {
+        if(Ins == null) {
+            Ins = this;
+        } else if(Ins != this) {
+            Destroy(this.gameObject);
+            return;
+        }
+    }
+
+    public void PlayMusicOnce(AudioClip clip) {
+        musicPlayer.Stop();
+        musicPlayer.loop = false;
+        musicPlayer.PlayOneShot(clip);
+    }
+
+    public void PlayMusicLoop(AudioClip clip) {
+        musicPlayer.Stop();
+        musicPlayer.loop = true;
+        musicPlayer.clip = clip;
+        musicPlayer.Play();
+    }
+
+    public void PlaySFXOnce(AudioClip clip) {
+        sfxPlayer.Stop();
+        sfxPlayer.loop = false;
+        sfxPlayer.PlayOneShot(clip);
+    }
+
+    public void PlaySFXLoop(AudioClip clip) {
+        sfxPlayer.Stop();
+        sfxPlayer.loop = true;
+        sfxPlayer.clip = clip;
+        sfxPlayer.Play();
+    }
+
+    public void StopMusic() {
+        musicPlayer.Stop();
+    }
+
+    public void StopSFX() {
+        sfxPlayer.Stop();
+    }
+}
