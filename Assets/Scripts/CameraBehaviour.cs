7e7a51466283b1dbe84882a72a7305cb1530a86b
Ahmet Hakan Koç <ahmetkoc.business@gmail.com>
2024-02-03 16:02:03 +0100

    Camera follow player in bounds

---
+++ Assets/Scripts/CameraBehaviour.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour {
+    private const float deadzonePositionLeft = -8.65f;
+    private const float deadzonePositionRight = 8.65f;
+    private const float deadzonePositionUp = 3.48f;
+    private const float deadzonePositionDown = -5.8f;
+
+    [SerializeField] private Transform playerTransform;
+
+    private void LateUpdate() {
+        FollowPlayerWithinBounds();
+    }
+
+    private void FollowPlayerWithinBounds() {
+        float clampedX = Mathf.Clamp(playerTransform.position.x, deadzonePositionLeft, deadzonePositionRight);
+        float clampedY = Mathf.Clamp(playerTransform.position.y, deadzonePositionDown, deadzonePositionUp);
+
+        this.transform.position = new Vector3(clampedX, clampedY, this.transform.position.z);
+    }
+}
