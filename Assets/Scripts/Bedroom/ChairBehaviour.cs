ee891d639a8ada9e0fc98e8ade9d6d1ea4203ed5
Ahmet Hakan Koç <ahmetkoc.business@gmail.com>
2024-02-05 15:52:36 +0100

    Various fixes

---
+++ Assets/Scripts/Bedroom/ChairBehaviour.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class ChairBehaviour : MonoBehaviour {
+    [SerializeField] private Sprite chairEmpty;
+    [SerializeField] private Sprite chairKid;
+
+    //TODO: Change chair sprite depending on whether the player is sitting or not
+}
